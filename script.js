5467d71de333fda7d2adbdfddcb0fe4a88339d53
PABLO AFONSO CASTRO RODRIGUES <130113391+pabloscriptgame@users.noreply.github.com>
2026-01-06 11:51:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ const menuData = {
     jantinha: {
         title: "🍲 Jantinhas",
         items: [
-            { name: "Jantinha", price: 12.00, img: "https://iili.io/fjUDoDQ.png", desc: "Arroz branco, Feijão de Caldo, Meio de asa assada, Salpicaão, Batata frita" }
+            { name: "2 Jantinha", price: 24.00, img: "https://iili.io/fjUDoDQ.png", desc: "Arroz branco, Feijão de Caldo, Meio de asa assada, Salpicaão, Batata frita" }
         ]
     }
 };
